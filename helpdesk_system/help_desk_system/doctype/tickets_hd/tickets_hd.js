4e92462c1a1d3eb73b4647c69f355a9edc8fecaa
kameshsekaran <kameshsk2020@gmail.com>
2026-07-09 15:52:02 +0530

    Task 4.3: Add ticket auto-fill and latest database backup

--- helpdesk_system/help_desk_system/doctype/tickets_hd/tickets_hd.js
+++ helpdesk_system/help_desk_system/doctype/tickets_hd/tickets_hd.js
@@ -1,28 +1,25 @@
 // Copyright (c) 2026, mahalakshmi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Tickets Hd", {
-// 	refresh(frm) {
-
-// 	},
-// });
 frappe.ui.form.on("Tickets Hd", {
+	refresh(frm) {
+
+	},
+
     onload: function(frm) {
-        frappe.call({
-            method: "frappe.client.get_list",
-            args: {
-                doctype: "Customer hd",
-                filters: {
-                    user: frappe.session.user
-                },
-                fields: ["name"],
-                limit_page_length: 1
-            },
-            callback: function(r) {
-                if (r.message.length) {
-                    frm.set_value("customer", r.message[0].name);
-                }
-            }
-        });
-    }
+        if(frm.is_new() && frappe.session.user != "Administrator"){
+            _get_customer(frm);
+        }
+    },
 });
+
+function _get_customer(frm){
+    customer = frappe.db.get_value("Customer hd", {"email_id": frappe.session.user}, "name").then(r => {
+        if(r.message){
+            
+            frm.set_value("customer", r.message.name);
+            // frm.set_df_property("customer", "read_only", 1); 
+        }
+    });
+}
+
